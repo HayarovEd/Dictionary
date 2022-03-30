e843f65e9fa4da1e076961a06cb2efb7ac5b14c4
hayarovy <edurd@rambler.ru>
2022-03-30 15:24:36 +0500

    released coroutines - dont work

--- app/src/main/java/com/edurda77/dictionary/App.kt
+++ app/src/main/java/com/edurda77/dictionary/App.kt
@@ -1,35 +1,10 @@
 package com.edurda77.dictionary
 
 import android.app.Application
-import com.edurda77.dictionary.dicoin.*
-import org.koin.android.ext.koin.androidContext
-import org.koin.core.context.startKoin
+import com.edurda77.dictionary.model.datasource.CaseRepoImpl
+import com.edurda77.dictionary.presenter.Presenter
 
 class App : Application() {
-    override fun onCreate() {
-        super.onCreate()
-        startKoin {
-            androidContext(this@App)
-            modules(listOf(viewModelModule, apiModule, netModule, repositoryModule))
-        }
-    }
-
-}
-/* Dagger - class App : Application(), HasAndroidInjector{
-    @Inject
-    lateinit var dispatchingAndroidInjector: DispatchingAndroidInjector<Any>
-    override fun androidInjector(): AndroidInjector<Any> {
-        return dispatchingAndroidInjector
-    }
-    override fun onCreate() {
-        super.onCreate()
-        DaggerAppComponent.builder()
-            .application(this)
-            .build()
-            .inject(this)
-    }
-} */
-/* MVP - class App : Application() {
     lateinit var presenterMainActivity: Presenter
     override fun onCreate() {
         super.onCreate()
@@ -41,4 +16,4 @@ class App : Application() {
         lateinit var instance: App
             private set
     }
-}*/
+}
--- app/src/main/java/com/edurda77/dictionary/view/MainActivity.kt
+++ app/src/main/java/com/edurda77/dictionary/view/MainActivity.kt
@@ -5,41 +5,29 @@ import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.edurda77.dictionary.App
 import com.edurda77.dictionary.databinding.ActivityMainBinding
 import com.edurda77.dictionary.model.data.WordTranslate
-import com.edurda77.dictionary.viewmodel.MainActivityViewModel
-import org.koin.androidx.viewmodel.ext.android.viewModel
 
-class MainActivity : AppCompatActivity() /*, BaseMainActivity without DI*/ {
+class MainActivity : AppCompatActivity(), BaseMainActivity {
     private lateinit var binding: ActivityMainBinding
 
-    private val viewModel: MainActivityViewModel by viewModel()
-    //@Inject
-    //Dagger - internal lateinit var viewModelFactory: ViewModelProvider.Factory
-    //Dagger - lateinit var viewModel: MainActivityViewModel
-    /*private val viewModel: MainActivityViewModelContract.ViewModel by lazy {
-        ViewModelProvider(this)[MainActivityViewModel::class.java]
-    } without DI */
-    //private val presenter = App.instance.presenterMainActivity MVP
-    //private var dataInput = emptyList<WordTranslate>().toMutableList() MVP
+    private val presenter = App.instance.presenterMainActivity
+    private var dataInput = emptyList<WordTranslate>().toMutableList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityMainBinding.inflate(layoutInflater)
-        //Dagger - AndroidInjection.inject(this)
 
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
-        //Dagger - viewModel = viewModelFactory.create(MainActivityViewModel::class.java)
-        //presenter.attachView(this) MVP
+
+        presenter.attachView(this)
 
         binding.search.setOnClickListener {
             val searchWord = binding.enter.text.toString()
-            viewModel.getData(searchWord)
-            //presenter.getData(searchWord.text.toString()) MVP
+            presenter.getData(searchWord)
         }
-        viewModel.liveData.observe(this) {
-            setOotRecycledView(it)
-        }
+
     }
 
     private fun setOotRecycledView(list: List<WordTranslate>) {
@@ -52,21 +40,21 @@ class MainActivity : AppCompatActivity() /*, BaseMainActivity without DI*/ {
         recyclerView.adapter = TranslateAdapter(list)
     }
 
-    /*MVP - override fun loadData(wordTranslate: List<WordTranslate>) {
+    override fun loadData(wordTranslate: List<WordTranslate>) {
         dataInput.clear()
         Thread {
             wordTranslate.forEach {
                 dataInput.add(it)
             }
             runOnUiThread {
-                setOotRecycledView()
+                setOotRecycledView(dataInput)
             }
         }.start()
-    }*/
+    }
 
-    /*override fun onStop() {
+    override fun onStop() {
         presenter.detachView(this)
         super.onStop()
-    }*/
+    }
 
 }
