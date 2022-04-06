8967a13ca40115b0fdc460079fac2686aac124a0
hayarovy <edurd@rambler.ru>
2022-04-06 10:47:36 +0500

    add load data to Db

--- app/src/main/java/com/edurda77/dictionary/view/HistoryActivity.kt
+++ app/src/main/java/com/edurda77/dictionary/view/HistoryActivity.kt
@@ -4,12 +4,18 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.edurda77.dictionary.databinding.ActivityHistoryBinding
 import com.edurda77.dictionary.databinding.ActivityMainBinding
+import com.edurda77.dictionary.viewmodel.HistoryActivityViewModel
+import com.edurda77.dictionary.viewmodel.MainActivityViewModel
+import org.koin.androidx.viewmodel.ext.android.viewModel
 
 class HistoryActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHistoryBinding
+    private val viewModel: HistoryActivityViewModel by viewModel()
     override fun onCreate(savedInstanceState: Bundle?) {
         binding = ActivityHistoryBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        binding.historyCurrent.text
+        viewModel.getAllData()
     }
 }
